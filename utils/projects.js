39e6b9431b25a11cf3c82465370e446ff51529e3
Kashish Behl <kashishbehl36@gmail.com>
2022-05-28 20:26:40 +0530

    Sidebar faded background added | outside click to sidebar | image to take width as per aspect | cursor fixed | thumbnails added correctly

--- utils/projects.js
+++ utils/projects.js
@@ -46,10 +46,9 @@ const projects = [
     ],
     gallery: [
       "/projects/attitude-makeover/Custom Size – 4.jpg",
+      "/projects/attitude-makeover/Custom Size – 5.jpg",
       "/projects/attitude-makeover/Custom Size – 6.jpg",
       "/projects/attitude-makeover/Custom Size – 7.jpg",
-      "/projects/attitude-makeover/Custom Size – 5.jpg",
-      "/projects/attitude-makeover/Custom Size – 8.jpg",
     ],
     socialMedia: {
       insta: "",
@@ -79,11 +78,11 @@ const projects = [
       "/projects/interweave/Custom Size – 16.jpg",
     ],
     gallery: [
-      "/projects/interweave/Custom Size – 22.jpg",
       "/projects/interweave/Custom Size – 17.jpg",
       "/projects/interweave/Custom Size – 19.jpg",
       "/projects/interweave/Custom Size – 20.jpg",
       "/projects/interweave/Custom Size – 21.jpg",
+      "/projects/interweave/Custom Size – 22.jpg",
     ],
     socialMedia: {
       insta: "",
@@ -105,10 +104,11 @@ const projects = [
     description: "Hired to create their brand identity and all forms of visual communication before and during the exclusive yoga event to attract participants and donations. We were indeed very happy that our partnership raised INR1,26,625 for Covid Relief in India in less than 2 weeks!",
     coverImage: "/projects/flex-for-good/Custom Size – 10.jpg",
     thumbnails: [
+      "/projects/flex-for-good/Custom Size – 8.jpg",
       "/projects/flex-for-good/Custom Size – 9.jpg",
-      "/projects/flex-for-good/Custom Size – 11.jpg",
     ],
     gallery: [
+      "/projects/flex-for-good/Custom Size – 11.jpg",
       "/projects/flex-for-good/Custom Size – 12.jpg",
       "/projects/flex-for-good/Custom Size – 13.jpg",
       "/projects/flex-for-good/Custom Size – 14_1.mp4",
@@ -134,14 +134,14 @@ const projects = [
     description: "Neck Up (previously Neck Up Australia) is a US-based premium men’s grooming brand that caters to US and Australia. Their goal is to become a brand that attracts men who are just starting to explore grooming and skincare. We rebranded and redesigned their packaging, along with the talented Kevin Angelus, to place Neck Up as a stand-out brand who is not just for the ‘manly’ man.",
     coverImage: "/projects/neckup/Custom Size – 24.jpg",
     thumbnails: [
-      "/projects/neckup/Custom Size – 23.jpg",
-      "/projects/neckup/Custom Size – 25.jpg",
+      "/projects/neckup/Custom Size – 29.jpg",
+      "/projects/neckup/Custom Size – 28.jpg",
     ],
     gallery: [
+      "/projects/neckup/Custom Size – 23.jpg",
+      "/projects/neckup/Custom Size – 25.jpg",
       "/projects/neckup/Custom Size – 26.jpg",
       "/projects/neckup/Custom Size – 27.jpg",
-      "/projects/neckup/Custom Size – 28.jpg",
-      "/projects/neckup/Custom Size – 29.jpg",
     ],
     socialMedia: {
       insta: "",
@@ -167,11 +167,11 @@ const projects = [
       "/projects/bolly-coin/Custom Size – 39.jpg",
     ],
     gallery: [
-      "/projects/bolly-coin/Custom Size – 45.jpg",
       "/projects/bolly-coin/Custom Size – 41.jpg",
       "/projects/bolly-coin/Custom Size – 42.jpg",
       "/projects/bolly-coin/Custom Size – 43.jpg",
       "/projects/bolly-coin/Custom Size – 44.jpg",
+      "/projects/bolly-coin/Custom Size – 45.jpg",
     ],
     socialMedia: {
       insta: "",
@@ -193,15 +193,15 @@ const projects = [
     description: "Easily one of our favorite client, divya napa n co. is a social media marketing agency run by a husband-wife duo (and their two adorable dogs). We create all kinds of Instagram collateral for them following the visual moodboard we created for their brand.",
     coverImage: "/projects/dnc/Custom Size – 47.jpg",
     thumbnails: [
-      "/projects/dnc/Custom Size – 46.jpg",
       "/projects/dnc/Custom Size – 51.jpg",
+      "/projects/dnc/Custom Size – 46.jpg",
     ],
     gallery: [
-      "/projects/dnc/Custom Size – 50.jpg",
-      "/projects/dnc/Custom Size – 48.jpg",
       "/projects/dnc/Custom Size – 52.jpg",
-      "/projects/dnc/Custom Size – 49_1.mp4",
+      "/projects/dnc/Custom Size – 48.jpg",
       "/projects/dnc/Custom Size – 53_1.mp4",
+      "/projects/dnc/Custom Size – 49_1.mp4",
+      "/projects/dnc/Custom Size – 50.jpg",
     ],
     socialMedia: {
       insta: "",
@@ -229,8 +229,8 @@ const projects = [
     gallery: [
       
       "/projects/sundaze/Custom Size – 57.jpg",
-      "/projects/sundaze/Custom Size – 58.jpg",
       "/projects/sundaze/Custom Size – 59.jpg",
+      "/projects/sundaze/Custom Size – 58.jpg",
       "/projects/sundaze/Custom Size – 60.jpg",
       "/projects/sundaze/Custom Size – 61.jpg",
     ],
